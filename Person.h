1130f765cd0def0efee60a8c05490907a421703d
Elijah Ngo <161224816+Techngo10@users.noreply.github.com>
2024-05-14 14:32:02 +0930

    Fix
    I fixed the error

--- Person.h
+++ Person.h
@@ -1,33 +1,33 @@
 #ifndef PERSON_H
 #define PERSON_H
 
+#include <ctime>
 #include <iostream>
 #include <string>
-#include <ctime>
 
 class Person {
-    private:
-        int age;
-        int phone_number;
-        std::string name;
-        std::string address;
+private:
+  int age;
+  int phone_number;
+  std::string name;
+  std::string address;
 
-    public:
-        Person();
-        Person(int new_age, int new_phone, std::string new_name, std::string new_address);
+public:
+  Person();
+  Person(int new_age, int new_phone, std::string new_name,
+         std::string new_address);
 
-        int get_age();
-        void set_age(int new_age);
+  int get_age();
+  void set_age(int new_age);
 
-        int get_phone();
-        void set_phone(int new_phone);
+  int get_phone();
+  void set_phone(int new_phone);
 
-        std::string get_name();
-        void set_name(std::string new_name);
+  std::string get_name();
+  void set_name(std::string new_name);
 
-        std::string get_address();
-        void set_address(std::string new_address);
-
+  std::string get_address();
+  void set_address(std::string new_address);
 };
 
 #endif
--- Person_test.cpp
+++ Person_test.cpp
@@ -11,10 +11,10 @@ int main(){
     std::cout << "Address: " << Person1.get_address() << std::endl;
 
     Person Person2(30, 1234567890, "John", "I Live Here");
-    std::cout << "Name: " << Person1.get_name() << std::endl;
-    std::cout << "Age: " << Person1.get_age() << std::endl;
-    std::cout << "Phone Number: " << Person1.get_phone() << std::endl;
-    std::cout << "Address: " << Person1.get_address() << std::endl;
+    std::cout << "Name: " << Person2.get_name() << std::endl;
+    std::cout << "Age: " << Person2.get_age() << std::endl;
+    std::cout << "Phone Number: " << Person2.get_phone() << std::endl;
+    std::cout << "Address: " << Person2.get_address() << std::endl;
 
     return 0;
 }
